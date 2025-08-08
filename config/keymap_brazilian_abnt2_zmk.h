3a8aebae13da7d8644ca4798c7f254948c1b105d
RaphaelRossi <raphaelhrossi@hotmail.com>
2025-08-08 18:26:44 -0300

    Fix question mark key mapping in Brazilian ABNT2 layout

--- config/keymap_brazilian_abnt2_zmk.h
+++ config/keymap_brazilian_abnt2_zmk.h
@@ -152,7 +152,7 @@ Layout ABNT2 de referência:
 #define BR_PIPE &kp LS(NUBS) // | (pipe)
 #define BR_LABK &kp LS(COMMA) // < (menor que)
 #define BR_RABK &kp LS(DOT)   // > (maior que)
-#define BR_QUES &kp LS(W)     // ? (interrogação) - posição correta no ABNT2
+#define BR_QUES &kp LS(MINUS) // ? (interrogação) - posição correta no ABNT2
 
 // CARACTERES COM ALTGR (right alt)
 #define BR_SUP2 &kp RA(N2)   // ² (dois ao quadrado)
