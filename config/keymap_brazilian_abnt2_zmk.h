42676fddeaeb89fdc46d96ceb8f70d07ca2cb450
raphael <raphaelhrossi@hotmail.com>
2026-01-05 16:09:19 -0300

    Trying another slash config for Linux

--- config/keymap_brazilian_abnt2_zmk.h
+++ config/keymap_brazilian_abnt2_zmk.h
@@ -145,9 +145,9 @@ Layout ABNT2 de referência:
 
 //#define BR_FSLH &slash_option_1   // NOVA OPÇÃO 1: Shift+7 (alguns layouts) dispara : &
 
-#define BR_FSLH &slash_option_2   // NOVA OPÇÃO 2: Alt+047 (código ASCII)  FUNCIONOU
+// #define BR_FSLH &slash_option_2   // NOVA OPÇÃO 2: Alt+047 (código ASCII)  FUNCIONOU
 // #define BR_FSLH &slash_option_3   // NOVA OPÇÃO 3: usando BSLH
-// #define BR_FSLH &slash_option_4   // NOVA OPÇÃO 4: AltGr+7 (ABNT2)
+#define BR_FSLH &slash_option_4   // NOVA OPÇÃO 4: AltGr+7 (ABNT2)
 // #define BR_FSLH &slash_option_5   // NOVA OPÇÃO 5: Sequência AltGr+7
 // #define BR_FSLH &slash_option_6   // NOVA OPÇÃO 6: usando EQUAL
 // #define BR_FSLH &slash_option_7   // NOVA OPÇÃO 7: Shift+Comma
